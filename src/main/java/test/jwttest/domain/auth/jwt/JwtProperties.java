7aab7d49933729ec427792511b1ac2af13ca5790
codevarty <hisu7789@gmail.com>
2024-10-16 23:39:21 +0900

    [ADD] add JwtProperties class

---
+++ src/main/java/test/jwttest/domain/auth/jwt/JwtProperties.java
@@ -0,0 +1,28 @@
+package test.jwttest.domain.auth.jwt;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+  private String issuer;
+  private String secret;
+
+  /**
+   * 인증키 반환 메소드
+   *
+   * @return SecretKey
+   */
+  public SecretKey getSecretKey() {
+    return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+  }
+}
